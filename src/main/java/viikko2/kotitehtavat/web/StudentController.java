cca584c5178bb9897b48bc26c35e2d99df75156c
kxista <krista.nousiainen@protukipiste.fi>
2025-09-02 18:19:37 +0300

    Lisätty Viikko 2 tehtävät

--- src/main/java/viikko2/kotitehtavat/web/StudentController.java
+++ src/main/java/viikko2/kotitehtavat/web/StudentController.java
@@ -1,8 +1,9 @@
 package viikko2.kotitehtavat.web;
 
-import org.springframework.stereotype.Controller;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
